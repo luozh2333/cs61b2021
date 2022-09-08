cb9976e7087ad3b47e1fa320690e844cc00841e6
luo <1931976629@qq.com>
2022-09-08 10:21:44 +0800

    lab6

--- lab6/capers/CapersRepository.java
+++ lab6/capers/CapersRepository.java
@@ -1,46 +1,65 @@
 package capers;
 
+//import com.sun.xml.internal.xsom.impl.UName;
+
 import java.io.File;
+
 import static capers.Utils.*;
 
-/** A repository for Capers 
- * @author TODO
- * The structure of a Capers Repository is as follows:
+/**
+ * A repository for Capers
  *
+ * @author The structure of a Capers Repository is as follows:
+ * <p>
  * .capers/ -- top level folder for all persistent data in your lab12 folder
- *    - dogs/ -- folder containing all of the persistent data for dogs
- *    - story -- file containing the current story
- *
- * TODO: change the above structure if you do something different.
+ * - dogs/ -- folder containing all of the persistent data for dogs
+ * - story -- file containing the current story
+ * <p>
  */
 public class CapersRepository {
-    /** Current Working Directory. */
+    static Utils ut = null;
+    /**
+     * Current Working Directory.
+     */
     static final File CWD = new File(System.getProperty("user.dir"));
 
-    /** Main metadata folder. */
-    static final File CAPERS_FOLDER = null; // TODO Hint: look at the `join`
-                                            //      function in Utils
+    /**
+     * Main metadata folder.
+     */
+    static final File CAPERS_FOLDER = join(CWD, ".capers");
+    //      function in Utils
 
     /**
      * Does required filesystem operations to allow for persistence.
      * (creates any necessary folders or files)
      * Remember: recommended structure (you do not have to follow):
-     *
+     * <p>
      * .capers/ -- top level folder for all persistent data in your lab12 folder
-     *    - dogs/ -- folder containing all of the persistent data for dogs
-     *    - story -- file containing the current story
+     * - dogs/ -- folder containing all of the persistent data for dogs
+     * - story -- file containing the current story
      */
     public static void setupPersistence() {
-        // TODO
+        CAPERS_FOLDER.mkdir();
+        Dog.DOG_FOLDER.mkdir();
     }
 
     /**
      * Appends the first non-command argument in args
      * to a file called `story` in the .capers directory.
+     *
      * @param text String of the text to be appended to the story
      */
     public static void writeStory(String text) {
-        // TODO
+        File StoryFile = join(CAPERS_FOLDER, "story");
+        String totalString;
+        if (!StoryFile.exists()) {
+            totalString = text;
+        } else {
+            totalString = Utils.readContentsAsString(StoryFile);
+            totalString += "\n" + text;
+        }
+        Utils.writeContents(StoryFile, totalString);
+        System.out.println(totalString);
     }
 
     /**
@@ -49,16 +68,25 @@ public class CapersRepository {
      * Also prints out the dog's information using toString().
      */
     public static void makeDog(String name, String breed, int age) {
-        // TODO
+        Dog newDog = new Dog(name, breed, age);
+        newDog.saveDog();
+        System.out.println(newDog.toString());
     }
 
     /**
      * Advances a dog's age persistently and prints out a celebratory message.
      * Also prints out the dog's information using toString().
      * Chooses dog to advance based on the first non-command argument of args.
+     *
      * @param name String name of the Dog whose birthday we're celebrating.
      */
     public static void celebrateBirthday(String name) {
-        // TODO
+        Dog birthdayDog = Dog.fromFile(name);
+        birthdayDog.haveBirthday();
+        birthdayDog.saveDog();
+    }
+    public static void HelloDog(String name){
+        Dog MyDog = Dog.fromFile(name);
+        System.out.println(MyDog.toString());
     }
 }
